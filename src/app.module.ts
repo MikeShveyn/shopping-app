95d96bcb62ae23b364d0e84780327e790f064d55
Michael <misha.shveynfurt@gmail.com>
2025-01-31 15:48:42 +0200

    INIT MAIN ROUTES and components with base strucutre, no logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ProductsModule, StatisticsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
